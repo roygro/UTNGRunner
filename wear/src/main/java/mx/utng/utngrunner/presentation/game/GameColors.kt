3b40e442537b21016fccd3d88ebca758d1efd133
roygro <princess18gro@gmail.com>
2026-06-17 23:39:59 -0600

    pequeño ajuste

---
+++ wear/src/main/java/mx/utng/utngrunner/presentation/game/GameColors.kt
@@ -0,0 +1,16 @@
+package mx.utng.utngrunner.presentation.game
+
+import androidx.compose.ui.graphics.Color
+
+class GameColors {
+    val player = Color(0xFFE65100)
+    val helmet = Color(0xFF1A237E)
+    val backgroundStart = Color(0xFF0D1B4A)
+    val backgroundEnd = Color(0xFF1A237E)
+    val ground = Color(0xFF3E2723)
+    val groundTop = Color(0xFF4E342E)
+    val coin = Color(0xFFFFD54F)
+    val coinBorder = Color(0xFFFFB300)
+    val text = Color.White
+    val textAccent = Color(0xFFF9A825)
+}
---
+++ wear/src/main/java/mx/utng/utngrunner/presentation/theme/Theme.kt
@@ -0,0 +1,13 @@
+package mx.utng.utngrunner.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun UTNGRunnerTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        content = content
+    )
+}
